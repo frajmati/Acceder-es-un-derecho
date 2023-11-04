0ea88d20386ad711ec738670f74286c969e7ed5c
frajmati <frajbergmatias@gmail.com>
2023-11-04 16:43:54 -0300

    Phone Bug

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //MENÚ
 
 const menuIcon = document.querySelector(".container__menuIcon");
-const markIcon = document.querySelector(".container__markIcon");
+// const markIcon = document.querySelector(".container__markIcon");
 const navMobile = document.querySelector(".nav__mobile");
 const navMobilelinks = document.querySelectorAll(".nav__ul--mobile .nav__li .nav__li--a");
 
@@ -10,11 +10,11 @@ menuIcon.addEventListener("click", () => {
     navMobile.classList.toggle("active");
 });
 
-markIcon.addEventListener("click", () => {
-    navMobile.classList.toggle("active");
-    document.body.style.overflow = "auto";
+// markIcon.addEventListener("click", () => {
+//     navMobile.classList.toggle("active");
+//     document.body.style.overflow = "auto";
     
-})
+// })
 
 navMobilelinks.forEach((link) => {
     link.addEventListener("click", () => {
